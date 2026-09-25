d0e1040d1e59630eae5135a967c815a29ee76a6c
Tetiana Shcherbyna <tshcherbinka@icloud.com>
2026-09-25 18:02:15 +0300

    Show active tab title and URL in popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }).then(([tab]) => {
+      if (tab) {
+        setTitle(tab?.title ?? "Назва недоступна");
+        setUrl(tab?.url ?? "Посилання недоступне");
+      }
+    });
+  }, []);
+
   return (
     <>
       <section>
-        <h1>Get started</h1>
+        <h1>{title}</h1>
+        <p>{url}</p>
       </section>
     </>
   );
